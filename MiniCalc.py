8d3ae2de66f09098a83a5f8fe3ec0ab4015c3145
FilipeJava <RM93042@fiap.com.br>
2022-03-24 22:45:18 -0300

    Mini Calculadora basica

---
+++ MiniCalc.py
@@ -0,0 +1,24 @@
+num1 =float(input("Digite o primeiro numero"))
+operador = input("digite um dos 5 operadores *,/,+,-,%")
+num2 =float(input("Digite o segundo numero"))
+
+if operador=="*":
+    resultado = num1*num2
+else:
+    if operador=="/":
+        if num2!=0:
+           resultado=num1/num2
+        else:
+            print("Impossivel divisão por zero")
+    else:
+        if operador=="+":
+            resultado=num1+num2 
+        else:
+            if operador=="-":
+                resultado=num1-num2
+            else:
+                resultado=num1%num2
+
+print(resultado)                
+                
+                                          

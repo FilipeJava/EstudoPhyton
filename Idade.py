4eb36241075e2c59cb717bf72de160430fb7aedf
FilipeJava <RM93042@fiap.com.br>
2022-03-24 21:59:10 -0300

    Usando comando de Seleção IF

---
+++ Idade.py
@@ -0,0 +1,9 @@
+anos = int(input("Digite sua idade"))
+
+if anos<18:
+    print("Voce é menor de idade")
+    print("Sua idade é :",anos)    
+
+else:
+    print("Voce é maior de idade ")
+    print("Sua idade é :",anos)

928c5ff8bf2ebf29c864b4556c65480ac2330649
FilipeJava <93042@fiap.com.br>
2022-05-02 19:37:19 -0300

    atualizado

--- CP_EX01.py
+++ CP_EX01.py
@@ -1,16 +1,20 @@
+
 entrada = int(input("Qual o tamanho da sequencia ?"))
 a1 = int(input("Digite a sequencia ?"))
 contador=0
 
-if(entrada ==1):
+if(entrada==1):
     while(entrada>1):
         a2 = int(input("Digite a sequencia ?"))
         if(a1==a2):
             contador = contador
+            a1=a2
         else:
-            contador = contador+1      
+            contador = contador+1  
+            a1=a2 
     entrada=entrada-1
 contador=1
+
 print(contador)
 
 

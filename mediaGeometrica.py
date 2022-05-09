cabf97547d59c614c51e4914b1aba747ba18a176
FilipeJava <93042@fiap.com.br>
2022-05-09 19:23:15 -0300

    Retorna Media Geometrica dois valores

---
+++ mediaGeometrica.py
@@ -0,0 +1,12 @@
+def mediaG (a,b):
+    result = (a*b)**(1/2)
+    
+    return result
+
+x = float(input("Digite o primeiro termo da média Geometrica: "))
+y = float(input("Digite o segundo termo da média Geometrica: "))
+
+
+print(mediaG(x,y))
+
+

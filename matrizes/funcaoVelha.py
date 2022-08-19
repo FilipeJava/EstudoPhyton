7be94243a64da6ac3e63e9d2a0dc1441abdcf86e
FilipeJava <RM93042@fiap.com.br>
2022-08-18 22:35:34 -0300

    velha construcao

---
+++ matrizes/funcaoVelha.py
@@ -0,0 +1,39 @@
+
+from pickle import FALSE
+
+
+def criaTabuleiro():
+    tabuleiro =[]
+    i=0
+    while i<3:
+        tabuleiro.append(['']*3)
+        i=i+1
+    return tabuleiro
+
+
+
+
+def haGanhador():
+    pass
+
+def temEspaco(tab):
+    resp =FALSE
+    i=0
+    while i<3:
+        j=0
+        while i<3:
+            
+    tab[0][1]==' '
+    
+
+def joga(tab,lin,col,jogador):
+    pass
+
+def trocaJogador(jogador):
+    if jogador == 'X':
+        return'O'
+    else:
+        return 'X'    
+
+def imprime(tab):
+    pass
